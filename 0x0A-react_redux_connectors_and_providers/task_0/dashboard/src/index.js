c511851f2d8f619c7e001a48a618ac82c730ffc7
dianaparr <dianaparra.9611@gmail.com>
2022-11-06 16:38:43 -0500

    add task 1

--- 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//createStore is deprecated!
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App/App";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+import { Map } from "immutable";
+
+const store = createStore(uiReducer, Map(initialState));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
